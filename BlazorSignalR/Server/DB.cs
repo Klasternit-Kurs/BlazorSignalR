b12ee4151ac1b7c94194026c6eaa730ba87cdad5
Dragan Pepic <d.pepic@hotmail.com>
2020-07-30 17:42:57 +0200

    Add project files.

---
+++ BlazorSignalR/Server/DB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSignalR.Server
+{
+	public class DB : DbContext
+	{
+		public DbSet<Shared.Proba> Probas { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-75VO5EN\TESTSERVER;Initial Catalog=TestBaza;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Shared.Proba>().HasKey(p => p.ID);
+		}
+	}
+}
---
+++ BlazorSignalR/Server/Hab.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSignalR.Server
+{
+	public class Hab : Hub
+	{
+		public void Probna(string s)
+		{
+			Console.WriteLine("Uspeh :) " + s);
+			Clients.Caller.SendAsync("zklj", 12);
+		}
+	}
+}
