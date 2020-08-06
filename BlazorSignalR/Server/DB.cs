22ec45463d65f682ce6e044b4c612facdeda44ab
Dragan Pepic <d.pepic@hotmail.com>
2020-08-06 17:50:29 +0200

    vdfvdfv

--- BlazorSignalR/Server/DB.cs
+++ BlazorSignalR/Server/DB.cs
@@ -8,8 +8,19 @@ namespace BlazorSignalR.Server
 {
 	public class DB : DbContext
 	{
-	
+
 		public DbSet<Shared.Proba> Probas { get; set; }
+
+		public DbSet<OtMA> OtMAs { get; set; }
+		public DbSet<OtMB> OtMBs { get; set; }
+
+		public DbSet<OtOA> OtOAs { get; set; }
+		public DbSet<OtOB> OtOBs { get; set; }
+
+		public DbSet<MtMA> MtMAs { get; set; }
+		public DbSet<MtMB> MtMBs { get; set; }
+		public DbSet<MtMA_B> MtMA_Bs { get; set; }
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-75VO5EN\TESTSERVER;Initial Catalog=TestBaza;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
@@ -17,6 +28,86 @@ namespace BlazorSignalR.Server
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<Shared.Proba>().HasKey(p => p.ID);
+
+			modelBuilder.Entity<OtMA>().HasKey(a => a.ID);
+			modelBuilder.Entity<OtMB>().HasKey(b => b.ID);
+
+			modelBuilder.Entity<OtOA>().HasKey(a => a.ID);
+			modelBuilder.Entity<OtOB>().HasKey(b => b.ID);
+
+			modelBuilder.Entity<MtMA>().HasKey(a => a.ID);
+			modelBuilder.Entity<MtMB>().HasKey(b => b.ID);
+			modelBuilder.Entity<MtMA_B>().HasKey(ab 
+				=> new {ab.IdA, ab.IdB });
+
+			modelBuilder.Entity<OtMA>().HasMany(a => a.lista)
+										.WithOne(b => b.A);
+
+			modelBuilder.Entity<OtOA>().HasOne(a => a.B)
+										.WithOne(b => b.A)
+										.HasForeignKey<OtOB>(b => b.IDA);
+
+			modelBuilder.Entity<MtMA_B>().HasOne(ab => ab.A)
+										.WithMany(a => a.listaAB)
+										.HasForeignKey(ab => ab.IdA);
+
+			modelBuilder.Entity<MtMA_B>().HasOne(ab => ab.B)
+										.WithMany(b => b.listaAB)
+										.HasForeignKey(ab => ab.IdB);
 		}
 	}
+
+	public class OtMA
+	{
+		public int ID { get; set; }
+		public List<OtMB> lista { get; set; } = new List<OtMB>();
+	}
+	public class OtMB
+	{
+		public int ID { get; set; }
+		public OtMA A { get; set; }
+	}
+
+	public class OtOA
+	{
+		public int ID { get; set; }
+		public OtOB B { get; set; } 
+	}
+	public class OtOB
+	{
+		public int ID { get; set; }
+		public OtOA A { get; set; }
+		public int IDA { get; set; }
+	}
+
+	public class MtMA
+	{
+		public int ID { get; set; }
+		public List<MtMA_B> listaAB { get; set; } = new List<MtMA_B>();
+	}
+
+	public class MtMB
+	{
+		public int ID { get; set; }
+		public List<MtMA_B> listaAB { get; set; } = new List<MtMA_B>();
+	}
+
+	public class MtMA_B
+	{
+		public int IdA { get; set; }
+		public int IdB { get; set; }
+
+		public MtMA A { get; set; }
+		public MtMB B { get; set; }
+
+		public MtMA_B(MtMA a, MtMB b)
+		{
+			A = a;
+			B = b;
+		}
+
+		public MtMA_B() { }
+	}
+
+
 }
--- BlazorSignalR/Server/Startup.cs
+++ BlazorSignalR/Server/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Linq;
 
 namespace BlazorSignalR.Server
@@ -15,7 +16,50 @@ namespace BlazorSignalR.Server
 			Configuration = configuration;
 
 			using (DB baza = new DB())
+			{
 				baza.Database.EnsureCreated();
+
+				OtMA a = new OtMA();
+				OtMB b1 = new OtMB();
+				OtMB b2 = new OtMB();
+
+				a.lista.Add(b1);
+				a.lista.Add(b2);
+
+				baza.OtMAs.Add(a);
+				baza.OtMBs.Add(b1);
+				baza.OtMBs.Add(b2);
+
+				baza.SaveChanges();
+
+				var nesto = baza.OtMBs.First();
+				Console.WriteLine(nesto.A);
+
+				OtOA ooa = new OtOA();
+				OtOB oob = new OtOB();
+				ooa.B = oob;
+
+				baza.OtOAs.Add(ooa);
+				baza.OtOBs.Add(oob);
+
+				baza.SaveChanges();
+
+				MtMA a1mm = new MtMA();
+				MtMA a2mm = new MtMA();
+				MtMB b1mm = new MtMB();
+				MtMB b2mm = new MtMB();
+
+				baza.MtMA_Bs.Add(new MtMA_B(a1mm, b1mm));
+				baza.MtMA_Bs.Add(new MtMA_B(a1mm, b2mm));
+				baza.MtMA_Bs.Add(new MtMA_B(a2mm, b1mm));
+				baza.MtMA_Bs.Add(new MtMA_B(a2mm, b2mm));
+				baza.MtMAs.Add(a1mm);
+				baza.MtMAs.Add(a2mm);
+				baza.MtMBs.Add(b1mm);
+				baza.MtMBs.Add(b2mm);
+
+				baza.SaveChanges();
+			}
 		}
 
 		public IConfiguration Configuration { get; }
